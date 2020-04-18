fa4491a7e17422644c389e0def4e788f3742e8a0
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2020-04-18 17:41:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 //Server
 const app = express();
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 //Middleware
